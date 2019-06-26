f9fc2abf278a570f4840bb74397a7edb524e57b8
y-funakawa <funa3260@gmail.com>
2019-06-26 22:06:17 +0900

    feat: エラーあり：バトル場とベンチのカードポジション設定

--- src/app/bench/bench.component.ts
+++ src/app/bench/bench.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { PokecaServiceService } from '../pokeca-service.service';
 import { Card } from '../card/card.component';
 import { SelectedCard, Place } from '../field/field.component';
+import { CardType } from '../card/card.component';
 
 @Component({
   selector: 'app-bench',
@@ -9,6 +11,7 @@ import { SelectedCard, Place } from '../field/field.component';
 })
 export class BenchComponent implements OnInit {
   @Input() bench: Card[];
+  @Input() benchType: Card[];
   @Input() benchIndex: number;
   @Input() selectedCard: SelectedCard;
   @Output() onClickBench = new EventEmitter<number>();
@@ -19,12 +22,32 @@ export class BenchComponent implements OnInit {
       (this.selectedCard.benchIndex === this.benchIndex || this.selectedCard.benchIndex === undefined)
     );
   }
-  constructor() {}
+
+  get notPokemon() {
+    return this.service.bench.filter(_ => _.type !== CardType.pokemon);
+  }
+
+  get pokemon() {
+    return this.service.bench.filter(_ => _.type === CardType.pokemon);
+  }
+
+  constructor(private service: PokecaServiceService) {}
 
   ngOnInit() {}
 
   onClick(e: Event, index: number) {
     e.stopPropagation();
     this.onClickBench.emit(index);
+  }
+
+  getClassType(type: CardType) {
+    switch (type) {
+      case CardType.pokemon:
+        return 'bench_card--pokemon';
+      case CardType.trainers:
+        return 'bench_card--tool';
+      case CardType.energy:
+        return 'bench_card--energie';
+    }
   }
 }
